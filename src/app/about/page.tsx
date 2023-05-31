1bfeb8c2f1a017adaaef27c57d10dd704286eac5
Artem Abramov <Artem_Abramov1@epam.com>
2023-05-31 21:25:52 +0300

    feat: convert Gallery to react components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
   // Replace with the actual image URL
-  const image = 'https://example.com/about-image.jpg';
+  const image = { src: 'https://example.com/about-image.jpg' };
 
   return <AboutPage title={title} content={content} image={image} />;
 }
--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,16 +1,27 @@
-<section>
-  <div className="container-fluid px-0">
-    <div className="text-center">
-      <h4 className="widget-title">My Gallery</h4>
-    </div>
-    <div className="row no-gutters">
-      {{range .gallery}}
-      <div className="col-lg-2 col-md-3 col-sm-4 col-6">
-        <a href="{{ .image | absURL}}" className="venobox" data-gall="gallery"><img className="img-fluid w-100"
-                                                                                    src="{{ .image | absURL}}"
-                                                                                    alt=""></a>
+export type GalleryProps = {
+  gallery: {
+    id: string;
+    imageURL: string;
+  }[];
+};
+
+export const Gallery = ({ gallery }: GalleryProps) => {
+  return (
+    <section>
+      <div className="container-fluid px-0">
+        <div className="text-center">
+          <h4 className="widget-title">My Gallery</h4>
+        </div>
+        <div className="row no-gutters">
+          {gallery.map((item) => (
+            <div key={item.id} className="col-lg-2 col-md-3 col-sm-4 col-6">
+              <a href={item.imageURL} className="venobox" data-gall="gallery">
+                <img className="img-fluid w-100" src={item.imageURL} alt="" />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
-      {{end}}
-    </div>
-  </div>
-</section>
+    </section>
+  );
+};
