c601ddf73b38bec292479da115963962e977d282
Artem Abramov <Artem_Abramov1@epam.com>
2023-06-29 00:19:43 +0300

    feat: pillar post

--- src/components/PillarPost/PillarPost.tsx
+++ src/components/PillarPost/PillarPost.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image';
 import { PostMetadata } from '@/models';
+import Link from 'next/link';
 import { formatPostDate } from '@/lib/utils';
+import { ensureLeadingSlash } from 'next/dist/shared/lib/page-path/ensure-leading-slash';
 
 export type PillarPostProps = {
   post: PostMetadata;
@@ -7,10 +10,16 @@ export type PillarPostProps = {
 
 export const PillarPost = ({ post }: PillarPostProps) => {
   return (
-    <div className="col-10 featured-post-slider">
+    <div className="col-10">
       <article className="card featured-post">
         <div className="row g-0 align-items-center">
-          <div className="col-md-5">{post.image && <img src={post.image} className="card-img" alt={post.title} />}</div>
+          <div className="col-md-5">
+            {post.image && (
+              <div className="pillar-img-wrapper">
+                <Image src={ensureLeadingSlash(post.image)} className="pillar-img" alt={post.title ?? ''} fill />
+              </div>
+            )}
+          </div>
           <div className="col-md-6 offset-md-1">
             <div className="card-body">
               <div className="mb-3 post-meta">
@@ -18,9 +27,9 @@ export const PillarPost = ({ post }: PillarPostProps) => {
                 <span className="border-bottom border-primary px-2 mx-1" />
                 <span>{formatPostDate(post.date)}</span>
               </div>
-              <a href={post.slug} className="h1 fw-bold d-block text-dark mb-4 card-title">
+              <Link href={post.slug} className="h1 fw-bold d-block text-dark mb-4 card-title">
                 {post.title}
-              </a>
+              </Link>
               <p className="card-text">{post.description}...</p>
             </div>
           </div>
